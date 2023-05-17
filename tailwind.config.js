3bbb8673cf4a56dfed8cd34943e17fb616145424
mrtripop <tripop.tor@qualint.io>
2023-05-18 01:41:07 +0700

    feat(tailwind): add theme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,26 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'dark-blue': '#002F47',
+      black: '#000000',
+      'dim-black': '#010101',
+      white: '#ffffff',
+      'dim-white': '#D9D9D9',
+      gray: '#E5E5E5',
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
