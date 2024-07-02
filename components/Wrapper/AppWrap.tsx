ffaccf8bed5d85b3f50c8850cbe8a2efabbefa33
mrtripop <tripop.chai12@gmail.com>
2024-07-03 00:21:22 +0700

    feat: container and motion wrapper

---
+++ components/Wrapper/AppWrap.tsx
@@ -0,0 +1,21 @@
+import { ComponentType } from 'react'
+import { NavigationDots, SocialMedia } from '../Navbar'
+
+const AppWrap = (
+  Component: ComponentType,
+  idName: string,
+  classNames?: string
+) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        {/* <SocialMedia /> */}
+        <div className="app__wrapper app__flex">
+          <Component />
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    )
+  }
+
+export default AppWrap
