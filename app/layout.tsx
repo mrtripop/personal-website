e3e9c90ac2c63617495a43244883de6ad8de8092
mrtripop <tripop.chai12@gmail.com>
2024-07-19 20:49:20 +0700

    feat(navbar): change tailwind to scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import { Lato } from 'next/font/google'
+import { Lato, Nunito } from 'next/font/google'
 import Navbar from '@components/Navbar/Navbar'
-import Footer from '@components/Footer/Footer'
+import Footer from '@components/Footer/NewFooter'
 
 import '@styles/global.css'
 import '@styles/global.scss'
 
-const latoFont = Lato({
+const fontLato = Lato({
   weight: ['100', '300', '400', '700', '900'],
   subsets: ['latin'],
-  variable: '--font-lato',
+  variable: '--lato-font',
+})
+
+const fontNunito = Nunito({
+  weight: ['200', '300', '400', '700', '900'],
+  subsets: ['latin'],
+  variable: '--nunito-font',
 })
 
 export const metadata = {
@@ -17,48 +23,43 @@ export const metadata = {
     'Portfolio website to improve skill, collect knowledge, sharing profile, and make good opportunities',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en' className={`${fontLato.variable} ${fontNunito.variable}`}>
       <head>
-        <meta property="og:title" content="Mrtripop World Landing" />
-        <meta property="og:type" content="article" />
+        <meta property='og:title' content='Mrtripop Portfolio' />
+        <meta property='og:type' content='article' />
         <meta
-          property="og:description"
-          content="Portfolio website to improve skill, collect knowledge, sharing profile, and make good opportunities"
+          property='og:description'
+          content='Portfolio website to collect experiences, showcase, and frontend practicing.'
         />
+        <meta property='og:url' content='https://mrtripop.github.io/personal-website' />
         <meta
-          property="og:url"
-          content="https://mrtripop.github.io/personal-website"
-        />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/mrtripop/personal-website/main/assets/personal-image.png"
+          property='og:image'
+          content='https://raw.githubusercontent.com/mrtripop/personal-website/main/assets/personal-image.png'
         />
 
-        <meta name="twitter:title" content="Mrtripop World Landing" />
+        <meta name='twitter:title' content='Mrtripop Portfolio' />
         <meta
-          name="twitter:description"
-          content="Portfolio website to improve skill, collect knowledge, sharing profile, and make good opportunities for me."
+          name='twitter:description'
+          content='Portfolio website to collect experiences, showcase, and frontend practicing.'
         />
         <meta
-          name="twitter:image"
-          content="https://raw.githubusercontent.com/mrtripop/personal-website/main/assets/personal-image.png"
+          name='twitter:image'
+          content='https://raw.githubusercontent.com/mrtripop/personal-website/main/assets/personal-image.png'
         />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name='twitter:card' content='summary_large_image' />
 
-        <link rel="icon" href="/icon.ico" sizes="any" />
+        <link rel='icon' href='/icon.ico' sizes='any' />
 
         <title>Mrtripop</title>
       </head>
-      <body className={latoFont.className}>
-        <Navbar />
-        <main className="scroll-smooth">{children}</main>
-        <Footer />
+      <body>
+        <main className='scroll-smooth'>
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   )
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,19 @@
+'use client'
 import IconsButton from '@components/Button/IconsButton'
 import { images } from '@assets'
+import BurgerButton from '@components/Button/BurgerButton'
+
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full h-[70px] flex items-center justify-center z-[99999] bg-dim-white bg-opacity-20 backdrop-blur-md">
-      <div className="container flex flex-wrap items-center justify-between w-full h-full px-8">
+    <nav className='nav-bar app__flex'>
+      <div className='nav-bar__container'>
         <IconsButton src={images.BoyImage.src} />
-        <div className="h-[45px] w-[119px] rounded-full bg-white shadow-lg flex items-center justify-center">
-          <h2 className="text-lg font-bold primary-gradient">Mrtripop</h2>
+        <div className='app__flex'>
+          <h2 className='primary-gradient'>Mrtripop</h2>
         </div>
-        <IconsButton src={images.BurgerIcon.src} />
+        <BurgerButton />
       </div>
     </nav>
   )
